3e3bfc3ed64057abb2ddc4560b1fc7cda2a4de7a
Petko D. Petkov <pdp@websecurify.com>
2022-09-14 15:15:25 +0000

    Formatting.

--- bin/httptrace.js
+++ bin/httptrace.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
 
-const cluster = require('cluster')
+import cluster from 'cluster'
+
+import HttpTrace from '../lib/index.js'
 
 if (cluster.isMaster) {
-    console.log(`* master ${process.pid} started`)
+  console.log(`* master ${process.pid} started`)
 
-    const cpus = require('os').cpus().length
+  const cpus = require('os').cpus().length
 
-    for (let i = 0; i < cpus; i++) {
-        cluster.fork()
-    }
-}
-else {
-    console.log(`* worker ${process.pid} started`)
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork()
+  }
+} else {
+  console.log(`* worker ${process.pid} started`)
 
-    const HttpTrace = require('../lib/index')
+  const server = new HttpTrace()
 
-    const server = new HttpTrace()
-
-    server.listen(process.env.PORT || 8080)
+  server.listen(process.env.PORT || 8080)
 }
